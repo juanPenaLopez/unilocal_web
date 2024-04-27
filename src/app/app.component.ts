6e890b149acfb888cadc9f4e64bc1587e317b2da
Juan Peña <juanp.penal@uqvirtual.edu.co>
2024-04-26 20:42:47 -0500

    Primer commit del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'unilocal_web';
+
+  footer = 'Universidad del Quindío - 2024-1';
 }
