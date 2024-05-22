7e9257df76e2706c72b4d63e4f18db8e77fb756a
David Martinez <davids.martinezl@uqvirtual.edu.co>
2024-05-21 21:54:07 -0500

    Se crea la vista "consultar-negocio" para moderador y usuario normal

---
+++ src/app/componentes/consultar-negocio/consultar-negocio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultar-negocio',
+  standalone: true,
+  imports: [],
+  templateUrl: './consultar-negocio.component.html',
+  styleUrl: './consultar-negocio.component.css'
+})
+export class ConsultarNegocioComponent {
+
+}
