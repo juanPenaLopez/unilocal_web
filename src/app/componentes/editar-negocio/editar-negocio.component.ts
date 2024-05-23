112acb5c402161f78e97cb48953e3cfe67197830
David Martinez <davids.martinezl@uqvirtual.edu.co>
2024-05-22 23:51:20 -0500

    Se crea la pantalla de "editar-negocio"

---
+++ src/app/componentes/editar-negocio/editar-negocio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-negocio',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-negocio.component.html',
+  styleUrl: './editar-negocio.component.css'
+})
+export class EditarNegocioComponent {
+
+}
