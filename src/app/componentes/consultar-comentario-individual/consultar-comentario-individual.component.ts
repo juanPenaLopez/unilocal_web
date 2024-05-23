b04d7553bb57502451ce11d96546c6dbfc10c3df
David Martinez <davids.martinezl@uqvirtual.edu.co>
2024-05-22 23:19:44 -0500

    Se crea la pantalla de "consultar-mentario-individual"

---
+++ src/app/componentes/consultar-comentario-individual/consultar-comentario-individual.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultar-comentario-individual',
+  standalone: true,
+  imports: [],
+  templateUrl: './consultar-comentario-individual.component.html',
+  styleUrl: './consultar-comentario-individual.component.css'
+})
+export class ConsultarComentarioIndividualComponent {
+
+}
