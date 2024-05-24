33e5876b8f872898886849c4689db1e4a5a437ff
David Martinez <davids.martinezl@uqvirtual.edu.co>
2024-05-24 01:17:13 -0500

    Se crea el TS de responder-comentario

--- src/app/componentes/responder-comentario/responder-comentario.component.ts
+++ src/app/componentes/responder-comentario/responder-comentario.component.ts
@@ -1,13 +1,31 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; 
+
+import { ComentarioDTO } from ComentarioDTO; 
 
 @Component({
   selector: 'app-responder-comentario',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './responder-comentario.component.html',
-  styleUrl: './responder-comentario.component.css'
+  styleUrls: ['./responder-comentario.component.css']
 })
 export class ResponderComentarioComponent {
+  comentarioForm: FormGroup; 
 
+  constructor(private fb: FormBuilder) { 
+    this.initForm(); 
+  }
+
+  initForm() {
+    this.comentarioForm = this.fb.group({
+      mensajeRespuesta: ['', Validators.required], 
+    });
+  }
+
+  
+  enviarRespuesta() {
+    if (this.comentarioForm.valid) { 
+      const respuesta: string = this.comentarioForm.value.mensajeRespuesta; 
+      // Aquí implementar la lógica que falta para enviar la respuesta del comentario
+    }
+  }
 }
