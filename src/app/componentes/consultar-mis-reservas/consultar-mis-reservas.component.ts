10d74e10d6853f318249b77f959c62d71aef0bfa
Juan Peña <juanp.penal@uqvirtual.edu.co>
2024-05-24 21:46:13 -0500

    Se ajustan vistas de editar lugar y mis reservas

--- src/app/componentes/consultar-mis-reservas/consultar-mis-reservas.component.ts
+++ src/app/componentes/consultar-mis-reservas/consultar-mis-reservas.component.ts
@@ -1,32 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ReservaDTO } from '../../dto/reserva.dto';
 import { ReservasService } from '../../servicios/reservas.service';
 import { CommonModule } from '@angular/common';
+import { TokenService } from '../../servicios/token.service';
+import { ModalInformacionComponent } from '../modal-informacion/modal-informacion.component';
 
 @Component({
   selector: 'app-consultar-mis-reservas',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ModalInformacionComponent],
   templateUrl: './consultar-mis-reservas.component.html',
   styleUrl: './consultar-mis-reservas.component.css'
 })
-export class ConsultarMisReservasComponent implements OnInit{
+export class ConsultarMisReservasComponent implements OnInit {
 
   ngOnInit(): void {
-    this.buscar(true);
+    this.buscar();
   }
 
   arrayReservas: Array<ReservaDTO> = [];
+  @ViewChild(ModalInformacionComponent) modalComponent: ModalInformacionComponent;
+  modalTitle: string;
+  modalContent: string;
 
-  constructor(private reservaService: ReservasService) {}
+  constructor(private reservaService: ReservasService,
+    private tokenService: TokenService
+  ) { }
 
-  private buscar(bandera: boolean){
-    if(bandera){
-      this.reservaService.consultarReservaPorUsuario("11").subscribe(resp => {
-        this.arrayReservas = resp;
-      }, error => {
-
-      });
+  private buscar() {
+    if (this.tokenService.isLogged()) {
+      const token = this.tokenService.getToken();
+      if (token) {
+        const tokenPayload = this.tokenService.decodePayload(token);
+        this.reservaService.consultarReservaPorUsuario(tokenPayload.id).subscribe(resp => {
+          if (resp) {
+            this.arrayReservas = resp;
+          }
+        })
+      } else {
+        this.modalTitle = "Error";
+        this.modalContent = "Se ha presentado un error técnico";
+        this.modalComponent.openModal();
+      }
     }
   }
 
